1bfe8f3921946cd3a8f4b44ec04934517d476b53
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2024-10-02 17:28:55 +0700

    refactor: chỉnh các list trong DataManager.
    * Thay vì khởi tạo list khi khởi tạo Data Manager, cho phép người dùng set list.

--- CourseManager/src/coursemanager/io/DataManager.java
+++ CourseManager/src/coursemanager/io/DataManager.java
@@ -6,19 +6,15 @@ import coursemanager.model.StudentList;
 
 public final class DataManager {
     private static DataManager instance = null;
-    private static CourseList courseList;
-    private static RegisterList registerList;
-    private static StudentList studentList;
+    private static CourseList courseList = null;
+    private static RegisterList registerList = null;
+    private static StudentList studentList = null;
 
     public static final String COURSE_SAVE_FILE = "courses.txt";
     public static final String REGISTER_SAVE_FILE = "registers.txt";
     public static final String STUDENT_SAVE_FILE = "students.txt";
 
-    private DataManager() {
-        courseList = new CourseList();
-        registerList = new RegisterList();
-        studentList = new StudentList();
-    }
+    private DataManager() {}
 
     public static DataManager getInstance() {
         if (instance == null) {
@@ -26,6 +22,18 @@ public final class DataManager {
             return instance;
         }
         return instance;
+    }
+
+    public static void setCourseList(CourseList courseList) {
+        DataManager.courseList = courseList;
+    }
+
+    public static void setRegisterList(RegisterList registerList) {
+        DataManager.registerList = registerList;
+    }
+
+    public static void setStudentList(StudentList studentList) {
+        DataManager.studentList = studentList;
     }
 
     public CourseList getCourseList() {
